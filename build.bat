1340c24f0012e7ce347b2becf3783aae58967a8b
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2021-02-18 22:40:06 +0100

    Hyfs

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 protoc --cpp_out=generated_src .\proto\engine.proto
-cmake -B .\build\ "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Hans/Documents/GitHub/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_BUILD_TYPE=Debug
+rem cmake -B .\build\ "-DCMAKE_TOOLCHAIN_FILE=C:/Users/Hans/Documents/GitHub/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_BUILD_TYPE=Debug
